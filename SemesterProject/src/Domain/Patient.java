2ee5820a2e4bd223de5ed2ee074a7f5e5675d55c
Danuja JAyawardhana <danujajayawar@gmail.com>
2015-07-23 11:04:34 +0530

    After defining patient variables

--- SemesterProject/src/Domain/Patient.java
+++ SemesterProject/src/Domain/Patient.java
@@ -1,5 +1,21 @@
 package Domain;
 
+import java.util.Date;
+
 class Patient {
+    private int PID;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private Date date;
+    private String gender;
+    private String address;
+    private String NIC;
+    private int patientContactNo;
+    private String nameOfTheGuardian;
+    private int guardianCinatactNo;
+    private String bloodGroup;
+    private String Allergies;
+    
     
 }
