cb49234f27930fb909dc06f06d9e388336070329
Toshiba <sahansamarasinghe.13@cse.mrt.ac.lk>
2015-08-07 15:25:11 +0530

    after some editing in labreport gui

--- SemesterProject/src/semesterproject/SemesterProject.java
+++ SemesterProject/src/semesterproject/SemesterProject.java
@@ -1,5 +1,6 @@
 package semesterproject;
 
+import gui.lab.LabReportGUI;
 import gui.login.ChangeLogInSetting;
 import gui.login.LoginFace;
 
@@ -9,6 +10,11 @@ public class SemesterProject {
    
     public static void main(String[] args) {
        //new ChangeLogInSetting().setVisible(true);
-       new LoginFace().setVisible(true);
+       //new LoginFace().setVisible(true);
+        LabReportGUI l=new LabReportGUI();
+        l.setVisible(true);
+        l.setResizable(false);
+        l.setTitle("Lab Report");
+        
     }
 }
