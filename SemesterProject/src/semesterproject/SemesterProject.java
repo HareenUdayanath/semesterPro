4682032846b05d94c43333a1d3d43f3efa4d906a
HareenUdayanath <hareenudayanath.13@cse.mrt.ac.lk>
2015-08-10 18:18:41 +0530

    5th 8/10/2015

--- SemesterProject/src/semesterproject/SemesterProject.java
+++ SemesterProject/src/semesterproject/SemesterProject.java
@@ -150,8 +150,8 @@ public class SemesterProject {
         r.setDate(Help.getDate(1992,10,5));
         ad.updateMedicalReport(r);*/
         
-        LabReport l = ad.getLastLabReport();
+        /*LabReport l = ad.getLastLabReport();
         l.setLabTechID(2);
-        ad.updateLabReport(l);
+        ad.updateLabReport(l*/
     }
 }
