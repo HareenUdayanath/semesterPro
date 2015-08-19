540a98ed1783ed0edfb941f77207944df5c5279f
HareenUdayanath <hareenudayanath.13@cse.mrt.ac.lk>
2015-08-19 13:58:17 +0530

    New1 8/19/2015

--- SemesterProject/src/gui/lab/ShowLabReportGUI.java
+++ SemesterProject/src/gui/lab/ShowLabReportGUI.java
@@ -184,11 +184,6 @@ public class ShowLabReportGUI extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosing(java.awt.event.WindowEvent evt) {
-                formWindowClosing(evt);
-            }
-        });
 
         detailPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Details of the test", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 12))); // NOI18N
 
@@ -694,17 +689,6 @@ public class ShowLabReportGUI extends javax.swing.JFrame {
     private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
         this.dispose();
     }//GEN-LAST:event_btnCancelActionPerformed
-
-    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
-                                  
-        if (JOptionPane.showConfirmDialog(this, "Are you sure you want to log out?","Confirm Action", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION){
-            this.dispose();
-            LoginFace l = new LoginFace();
-            l.setLocationRelativeTo(null);
-            l.setVisible(true);
-        }
-    
-    }//GEN-LAST:event_formWindowClosing
     
     /**
      * @param args the command line arguments
